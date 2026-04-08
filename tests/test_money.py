2fd041bab8fc4d5025fa7da6d75ee6e7425d4941
Al <albarrentine@gmail.com>
2026-04-08 01:55:02 -0400

    [fix] tests

--- tests/test_money.py
+++ tests/test_money.py
@@ -11,9 +11,9 @@ def test_money():
     assert m == "$100.00"
     assert m.amount == 100
     assert m.currency == "USD"
-    assert m.formatted.replace("\xa0", "") == "$100.00"
+    assert m.formatted == "$100.00"
     assert m.num_cents == 10000
-    assert m.friendly.replace("\xa0", "") == "$100"
+    assert m.friendly == "$100"
 
     assert Money.from_cents(10000, "USD") == "$100.00"
 
@@ -28,10 +28,10 @@ def test_money():
     assert Money("$100.00") <= Money("$100.01")
     assert Money("$100.00") <= Money("$100.00")
 
-    assert Money("$100.01").friendly.replace("\xa0", "") == "$100.01"
+    assert Money("$100.01").friendly == "$100.01"
 
-    assert str(Money("100", "EUR", "de_DE")) == "100,00\xa0€"
-    assert Money(100, "EUR", "de_DE").friendly == "100\xa0€"
+    assert str(Money("100", "EUR", "de_DE")) == "100,00€"
+    assert Money(100, "EUR", "de_DE").friendly == "100€"
 
     class Model(Schema):
         money: Money
@@ -51,6 +51,6 @@ def test_money():
     m = Model(money="100.01")
     m.money += "0.99"
     assert isinstance(m.money, Money)
-    assert m.to_json().replace("\xa0", "") == '{"money":"$101.00"}'
+    assert m.to_json() == '{"money":"$101.00"}'
 
     assert Money("") == 0
